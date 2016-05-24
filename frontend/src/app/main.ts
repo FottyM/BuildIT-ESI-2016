39d08cc094584906346f1d310a9d280346a9bac0
agaba <akaizat1@gmail.com>
2016-05-25 00:07:58 +0300

    material template started

--- frontend/src/app/main.ts
+++ frontend/src/app/main.ts
@@ -22,24 +22,127 @@ import {Invoices} from "./invoices/po-invoice";
   selector: 'app',
   directives: [ROUTER_DIRECTIVES],
   template: `
-    <nav>
-      <a [routerLink]="['PHRWizard']">Create PHR</a>
-       <a [routerLink]="['PHRListing']">List all PHR's</a>
-      <a [routerLink]="['POListing']">List all POs</a>
-      <a [routerLink]="['Login']">Login Page</a>
-    </nav>
-    <h1> WELCOME TO BUILD IT</h1>
+<section class="main-container">
+    <div class="container-fluid">
+
+
+        <div class="page-header filled img-bg">
+            <div class="overlay-bg">
+
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>BUILD IT </h2>
+                    <p>Construction meets perfect</p>
+                </div>
+                <div class="col-md-6">
+                    <ul class="list-page-breadcrumb">
+                        <li> <a [routerLink]="['PHRWizard']">Create PHR</a></li>
+                        <li><a [routerLink]="['PHRListing']">List all PHR's</a></li>
+                        <li class="active-page">  <a [routerLink]="['POListing']">List all POs</a></li>
+                        <li class="active-page">   <a [routerLink]="['Login']">Login Page</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+ <nav>
+     
+       
+     
     
-    <router-outlet></router-outlet>
+    </nav>
+   <router-outlet></router-outlet>
+
+
+
+
+
+    </div>
+    <!--Footer Start Here -->
+    <footer class="footer-container">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6 col-sm-6">
+                    <div class="footer-left">
+                        <span>© 2015 <a href="http://themeforest.net/user/westilian">westilian</a></span>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-6">
+                    <div class="footer-right">
+                        <span class="footer-meta">Crafted with&nbsp;<i class="fa fa-heart"></i>&nbsp;by&nbsp;<a href="http://themeforest.net/user/westilian">westilian</a></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--Footer End Here -->
+</section>
+   
+
+ <aside class="leftbar material-leftbar">
+    <div class="left-aside-container">
+        <div class="user-profile-container">
+            <div class="user-profile clearfix">
+                <div class="admin-user-thumb">
+                    <img src="assets/images/avatar/jaman_01.jpg" alt="admin">
+                </div>
+                <div class="admin-user-info">
+                    <ul>
+                        <li><a href="#">Site Engneer</a></li>
+                        <li><a href="#">siteengineer@buildit.com</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="admin-bar">
+                <ul>
+                    <li><a href="#"><i class="zmdi zmdi-power"></i>
+                    </a>
+                    </li>
+                    <li><a href="#"><i class="zmdi zmdi-account"></i>
+                    </a>
+                    </li>
+                    <li><a href="#"><i class="zmdi zmdi-key"></i>
+                    </a>
+                    </li>
+                    <li><a href="#"><i class="zmdi zmdi-settings"></i>
+                    </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <ul class="list-accordion">
+            <li class="list-title">Menus</li>
+            <li>
+                <a [routerLink]="['PHRWizard']" ><i class="zmdi zmdi-view-dashboard"></i><span class="list-label">Create PHR</span></a>
+                
+            </li>
+            <li class="list-title">Others</li>
+            <li>
+                <a [routerLink]="['POListing']"><i class="zmdi zmdi-view-web"></i><span class="list-label">Purchase Orders</span></a>
+                
+            </li>
+            <li>
+                <a [routerLink]="['PHRListing']"><i class="zmdi zmdi-check"></i><span class="list-label">Plant Hire Requests</span></a>
+                
+            </li>
+            <!--<li>-->
+                <!--<a [routerLink]="['Invoices']"><i class="zmdi zmdi-check"></i><span class="list-label">Plant Invoices</span></a>-->
+                <!---->
+            <!--</li>-->
+        </ul>
+    </div>
+</aside>
+    
   `
+
 })
 @RouteConfig([
   new Route({path: '/wizard', name: 'PHRWizard', component: PHRWizardComponent}),
   new Route({path: '/orders', name: 'POListing', component: POListingComponent}),
   new Route({path: '/phrs', name: 'PHRListing', component: PHRListingComponent}),
   new Route({path:'/login',name:'Login',component:LogInComponent}),
-  new Route({path:'/modify',name:'Modify',component:Modify}),
-  new Route({path:'/Invoices',name:'Invoices',component:Invoices})
+  new Route({path:'/modify',name:'Modify',component:Modify})
 
 
 ])
